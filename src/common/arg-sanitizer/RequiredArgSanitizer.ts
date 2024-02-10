1b70a6c0edf933c13b0174d04a46ca337f004df7
Thomas Ghys <thomas-ghys@outlook.com>
2024-02-10 11:02:00 +0100

    Add comments

--- src/common/arg-sanitizer/RequiredArgSanitizer.ts
+++ src/common/arg-sanitizer/RequiredArgSanitizer.ts
@@ -1,8 +1,10 @@
 import { ArgSanitizer } from "./ArgSanitizer";
 
 export function requiredArgSanitizer() {
+    // Base array of all the expected colors for the base theme generation
     const requiredArgs: string[] = ['primary', 'secondary', 'neutral', 'neutral-variant'];
 
+    // Loop through all the required args and Sanitize them
     requiredArgs.forEach((argument) => {
         ArgSanitizer(argument);
     })
--- src/common/tree-manipulation/createDirectory.ts
+++ src/common/tree-manipulation/createDirectory.ts
@@ -2,12 +2,14 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export function createDirectory(destinationPath: string) {
+    // An array of all the folders in the provided path
     let directoryList: string[] = [];
+    // The part of the path that has already been created
     let createdDestinationPath: string = '';
+    // The sanitized path to create
     const pathToCreate: string = sanitizeDirectoryPath(destinationPath);
 
-    
-
+    // Creates the array of folders to create no matter which file system is used.
     if (pathToCreate.includes('/')) {
         directoryList = pathToCreate.split('/');
     } else if (pathToCreate.includes('\\')) {
@@ -16,6 +18,7 @@ export function createDirectory(destinationPath: string) {
         directoryList.push(pathToCreate);
     }
 
+    // Loops through the directory list and creates the paths one by one if they do not yet exist
     directoryList.forEach((directoryPath: string) => {
         createdDestinationPath = path.join(createdDestinationPath, directoryPath);
 
@@ -27,6 +30,7 @@ export function createDirectory(destinationPath: string) {
     });
 }
 
+// Gets a path and splits off the root file path if it is provided
 function sanitizeDirectoryPath(directoryPath: string): string {
     if (directoryPath.includes(process.cwd())) {
         return directoryPath.replace(process.cwd(), '');
